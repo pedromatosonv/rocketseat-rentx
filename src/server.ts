f3948c2c6ad4734215743ccf5ddb8f92d25963ea
Pedro Matoso <pedroomatoso@gmail.com>
2022-02-08 02:49:06 -0300

    swagger

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,16 @@
 import express from 'express'
+import swaggerUi from 'swagger-ui-express'
+import YAML from 'yamljs'
+
 import { router } from './routes';
 
 const app = express();
 
 app.use(express.json())
+
+const swaggerFile = YAML.load('./src/swagger.yaml')
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile))
+
 app.use(router)
 
 app.listen(3333, () => console.log('Server listening on port 3333'))
