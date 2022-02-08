1dd1ab739ab757f23294560d9f10562b43f5add3
Pedro Matoso <pedroomatoso@gmail.com>
2022-02-08 00:06:04 -0300

    migração para estrutura useCase

--- src/routes/categoriesRoutes.ts
+++ src/routes/categoriesRoutes.ts
@@ -1,23 +1,15 @@
 import { Router } from 'express'
-import { CategoriesRepository } from '../modules/cars/repositories/CategoriesRepository'
-import { CreateCategoryService } from '../modules/cars/services/CreateCategoryService'
+import { createCategoryController } from '../modules/cars/useCases/createCategory'
+import { listCategoriesController } from '../modules/cars/useCases/listCategories'
 
 const categoriesRoutes = Router()
-const categoriesRepository = new CategoriesRepository()
 
 categoriesRoutes.post('/', (req, res) => {
-  const { name, description } = req.body
-  const createCategoryService = new CreateCategoryService(categoriesRepository)
-
-  createCategoryService.execute({ name, description })
-
-  return res.status(201).send()
+  createCategoryController.handle(req, res)
 })
 
 categoriesRoutes.get('/', (req, res) => {
-  const all = categoriesRepository.list()
-
-  return res.json(all)
+  listCategoriesController.handle(req, res)
 })
 
 export { categoriesRoutes }
